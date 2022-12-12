1497ae034f1e567710c0f7e5a02e2d05f2436011
HongQuan <lhquan244@gmail.com>
2022-12-12 22:53:45 +0700

    ++fix db

--- config-db.js
+++ config-db.js
@@ -3,9 +3,9 @@ const Pool = require("pg").Pool;
 const pool = new Pool({
   user: "postgres",
   password: "123456",
-  database: "postgres",
-  host: "localhost",
-  port: 5432,
+  database: "class-group",
+  host: "45.119.212.77",
+  port: 5001,
   //   ssl: {
   //     rejectUnauthorized: false,
   //   },
