c99d45e7161a5f554967a13966e8767b42a37bde
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2022-12-23 00:40:44 +0700

    update test

--- test.js
+++ test.js
@@ -1,7 +1,9 @@
 const sendGridMail = require("@sendgrid/mail");
 require("dotenv").config();
 console.log("process.env.SENDGRID_API_KEY: ", process.env.SENDGRID_API_KEY);
-sendGridMail.setApiKey(process.env.SENDGRID_API_KEY);
+sendGridMail.setApiKey(
+  "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"
+);
 
 function getMessage() {
   const body = "This is a test email using SendGrid from Node.js";
@@ -9,7 +11,7 @@ function getMessage() {
   return {
     to: "nguyenhuuan1902@gmail.com",
     from: "truongquocvuong1902@gmail.com",
-    subject: "Test email with Node.js and SendGrid",
+    subject: "Test email with Node.js and SendGrid 123",
     text: body,
     html: `<strong>${body}</strong>`,
   };
@@ -36,8 +38,8 @@ function getMessage() {
 const msg = {
   to: "nguyenhuuan1902@gmail.com",
   from: "truongquocvuong1902@gmail.com",
-  subject: "Sending with SendGrid is Fun",
-  text: "and easy to do anywhere, even with Node.js",
+  subject: "Sending with SendGrid is Fun 123",
+  text: "and easy to do anywhere, even with Node.js 123",
   html: "<strong>and easy to do anywhere, even with Node.js</strong>",
 };
 sendGridMail
