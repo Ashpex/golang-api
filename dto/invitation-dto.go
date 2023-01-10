ecb38758deb31f29158aa8b687966ba2ff3bb9a5
vuonghcmus <76389096+vuonghcmus@users.noreply.github.com>
2023-01-10 23:11:30 +0700

    delete

--- dto/invitation-dto.go
+++
@@ -1,6 +0,0 @@
-package dto
-
-type InvitationCreateDTO struct {
-	GroupID uint64 `json:"group_id" form:"group_id" binding:"required"`
-	Email   string `json:"email" form:"email" binding:"required"`
-}
